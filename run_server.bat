05cc051daafa05c95bda5bfa44f529770267ad5d
Ming Shing Su <mingshing.su@gmail.com>
2023-08-21 12:30:07 +0800

    run_server

---
+++ run_server.bat
@@ -0,0 +1,8 @@
+set PYTHONIOENCODING=utf-8
+set PYTHON=python
+set APIKEY=sk-CDTYxdG8vL3Qd0IqU65r
+set APIKEY2=T3BlbkFJzlBiaczs6ejYKP3JGnrY
+set CHARNAME=paimon
+set MODEL=gpt-3.5-turbo
+set PORT=38438
+%PYTHON% SocketServer.py --APIKey %APIKEY%%APIKEY2% --model %MODEL% --character %CHARNAME% --port %PORT% --chatVer 3 --brainwash False --stream True 
